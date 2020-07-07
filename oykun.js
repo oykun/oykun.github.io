11991d30edfd2f757d53059f3fcedb8bc18a330f
oykun <oykun.yilmaz@gmail.com>
2020-07-07 12:09:10 +0100

    removed js, type size fixes, single line for mailchimp

--- oykun.js
+++ oykun.js
@@ -5,27 +5,27 @@ var $clonedList = $list.clone();
 var listWidth = 20;
 
 $list.find("li").each(function (i) {
-			listWidth += $(this, i).outerWidth(true);
+  listWidth += $(this, i).outerWidth(true);
 });
 
 var endPos = $tickerWrapper.width() - listWidth;
 
 $list.add($clonedList).css({
-	"width" : listWidth + "px"
+  "width": listWidth + "px"
 });
 
 $clonedList.addClass("cloned").appendTo($tickerWrapper);
 
 //TimelineMax
-var infinite = new TimelineMax({repeat: -1, paused: true});
+var infinite = new TimelineMax({ repeat: -1, paused: true });
 var time = 20;
 
 infinite
-  .fromTo($list, time, {rotation:0.01,x:0}, {force3D:true, x: -listWidth, ease: Linear.easeNone}, 0)
-  .fromTo($clonedList, time, {rotation:0.01, x:listWidth}, {force3D:true, x:0, ease: Linear.easeNone}, 0)
-  .set($list, {force3D:true, rotation:0.01, x: listWidth})
-  .to($clonedList, time, {force3D:true, rotation:0.01, x: -listWidth, ease: Linear.easeNone}, time)
-  .to($list, time, {force3D:true, rotation:0.01, x: 0, ease: Linear.easeNone}, time)
+  .fromTo($list, time, { rotation: 0.01, x: 0 }, { force3D: true, x: -listWidth, ease: Linear.easeNone }, 0)
+  .fromTo($clonedList, time, { rotation: 0.01, x: listWidth }, { force3D: true, x: 0, ease: Linear.easeNone }, 0)
+  .set($list, { force3D: true, rotation: 0.01, x: listWidth })
+  .to($clonedList, time, { force3D: true, rotation: 0.01, x: -listWidth, ease: Linear.easeNone }, time)
+  .to($list, time, { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone }, time)
   .progress(1).progress(0)
   .play();
 
